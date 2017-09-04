090727643530ad37d10e42b46b9425ec435814fb
DESKTOP-N9TM13V\zhumeilu <824434649@qq.com>
2017-09-04 19:49:21 +0800

    no message

--- netty_2_server/src/_3_nio/TimeClientHandle.java
+++ netty_2_server/src/_3_nio/TimeClientHandle.java
@@ -1,8 +1,16 @@
 package _3_nio;
 
+import com.sun.org.apache.xpath.internal.SourceTree;
+
 import java.io.IOException;
+import java.net.InetSocketAddress;
+import java.nio.ByteBuffer;
+import java.nio.channels.SelectableChannel;
+import java.nio.channels.SelectionKey;
 import java.nio.channels.Selector;
 import java.nio.channels.SocketChannel;
+import java.util.Iterator;
+import java.util.Set;
 
 /**
  * Created by zhumeilu on 17/9/4.
@@ -28,6 +36,104 @@ public class TimeClientHandle implements Runnable{
     }
     @Override
     public void run() {
+
+        try{
+
+            doConnect();
+        }catch (Exception e){
+            e.printStackTrace();
+            System.exit(1);
+        }
+        while (!stop){
+            try{
+
+                selector.select(1000);
+                Set<SelectionKey> selectionKeys = selector.selectedKeys();
+                Iterator<SelectionKey> iterator = selectionKeys.iterator();
+                SelectionKey selectionKey = null;
+                while (iterator.hasNext()){
+                    selectionKey = iterator.next();
+                    iterator.remove();
+                    try{
+                        handleInput(selectionKey);
+                    }catch (Exception e){
+
+                        if(selectionKey!=null){
+                            selectionKey.cancel();
+                            if(selectionKey.channel()!=null){
+                                selectionKey.channel().close();
+                            }
+                        }
+                    }
+                }
+            }catch (Exception e){
+                e.printStackTrace();
+                System.exit(1);
+            }
+        }
+
+        if(selector!=null){
+            try{
+                selector.close();
+            }catch (Exception e){
+                e.printStackTrace();
+            }
+        }
+    }
+
+    private void handleInput(SelectionKey selectionKey) throws IOException {
+
+        if(selectionKey.isValid()){
+            SocketChannel sc = (SocketChannel) selectionKey.channel();
+            if(selectionKey.isConnectable()){
+                if(sc.finishConnect()){
+                    sc.register(selector,SelectionKey.OP_READ);
+                    doWrite(sc);
+                }else{
+                    System.exit(1);
+                }
+            }
+            if(selectionKey.isReadable()){
+                ByteBuffer readBuffer = ByteBuffer.allocate(1024);
+                int readBytes = sc.read(readBuffer);
+                if(readBytes>0){
+                    readBuffer.flip();
+                    byte[] bytes = new byte[readBuffer.remaining()];
+                    readBuffer.get(bytes);
+                    String body = new String(bytes,"utf-8");
+                    System.out.println("收到的消息："+body);
+                    this.stop = true;
+                }else if(readBytes<0){
+                    //对端链路关闭
+                    selectionKey.cancel();
+                    sc.close();
+                }else{
+                    //读到0字节，忽略
+                }
+            }
+        }
+
+    }
+
+    private void doWrite(SocketChannel sc) throws IOException {
+        byte[] req = "QUERY TIME ORDER".getBytes();
+        ByteBuffer writerBuffer = ByteBuffer.allocate(req.length);
+        writerBuffer.put(req);
+        writerBuffer.flip();
+        sc.write(writerBuffer);
+        if(!writerBuffer.hasRemaining()){
+            System.out.println("发送命令到服务器成功");
+        }
+    }
+
+    private void doConnect() throws IOException {
+
+        if(socketChannel.connect(new InetSocketAddress(host,port))){
+            socketChannel.register(selector,SelectionKey.OP_READ);
+            doWrite(socketChannel);
+        }else{
+            socketChannel.register(selector,SelectionKey.OP_CONNECT);
+        }
 
     }
 }
